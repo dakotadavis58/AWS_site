1d606d1dc855abcb767650a6f53c5e5dc7f20e65
StoneColdStutter <79773849+StoneColdStutter@users.noreply.github.com>
2021-11-08 13:03:10 -0500

    adding all files

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("hello");
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.get("/add", (req, res) => {
-    // let v1 = req.query.v1;
-    // let v2 = req.query.v2;
+  // let v1 = req.query.v1;
+  // let v2 = req.query.v2;
 
-    let {v1,v2} = req.query;
+  let { v1, v2 } = req.query;
 
-    let sum = parseInt(v1) + parseInt(v2);
-    res.json(sum);
+  let sum = parseInt(v1) + parseInt(v2);
+  res.json(sum);
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log("Server running on port 3000"));
